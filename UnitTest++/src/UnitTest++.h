8a93c9314389911004894fb41557109b62b90224
unknown <pbarthe@PC-PBA.PBA>
2012-12-13 09:51:41 +0100

    .

--- UnitTest++/src/UnitTest++.h
+++ UnitTest++/src/UnitTest++.h
@@ -3,16 +3,16 @@
 
 //lint -esym(1509,*Fixture)
 
-#include "Config.h"
-#include "Test.h"
-#include "TestList.h"
-#include "TestSuite.h"
-#include "TestResults.h"
+#include <unittest-cpp/Config.h>
+#include <unittest-cpp/Test.h>
+#include <unittest-cpp/TestList.h>
+#include <unittest-cpp/TestSuite.h>
+#include <unittest-cpp/TestResults.h>
 
-#include "TestMacros.h"
+#include <unittest-cpp/TestMacros.h>
 
-#include "CheckMacros.h"
-#include "TestRunner.h"
-#include "TimeConstraint.h"
+#include <unittest-cpp/CheckMacros.h>
+#include <unittest-cpp/TestRunner.h>
+#include <unittest-cpp/TimeConstraint.h>
 
 #endif
